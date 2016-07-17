8a0091ae6b6223280614a6c5031fd2ebc3d7dd6c
David <david.marin.calleja@gmail.com>
2016-07-17 22:30:29 +0200

    functions, closure

---
+++ code/methods-functions.swift
@@ -0,0 +1,3 @@
+func foo (bar: String) -> String {
+  return "parameter: \(bar)"
+}
--- code/the-block-closure-statement.m
+++ code/the-block-closure-statement.m
@@ -1,3 +1,13 @@
+// Basic form
+
+
 [self.dataManager completionBlock: ^(NSArray* todoItems) {
                     NSLog(@"block");
                   }];
+
+// Avoid retain cycle
+@property (nonatomic, strong) void (^block)();
+__weak typeof(self) welf = self;
+self.block = ^ {
+     [weakSelf doSecond];
+ };
