e33a1069254926f09ef447da6767d3efe57d2b6d
David <david.marin.calleja@gmail.com>
2016-07-19 10:03:55 +0200

    Types in objective-c

---
+++ code/Types.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+@implementation MyListOfTypes
+
+-(void) listOfTypes {
+    int       intValue      = 3;
+    NSNumber  *integerValue = @79;
+    NSString  *stringValue  = @"hello";
+}
+
+@end
