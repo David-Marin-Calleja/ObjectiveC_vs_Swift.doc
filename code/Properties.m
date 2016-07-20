0a8576d9f9b1c88e6670793d5b9e91ed044acea9
David <david.marin.calleja@gmail.com>
2016-07-20 08:15:31 +0200

    update properties in objective-c: nullable, non null, null_unspecified

--- code/Properties.m
+++ code/Properties.m
@@ -2,3 +2,8 @@
 @property (strong)    NSString *foo2;
 @property (weak)      NSString *foo3;
 @property (readonly)  NSString *foo4;
+
+// https://medium.com/the-traveled-ios-developers-guide/objective-c-in-2015-3cb7dab3690c#.457shxv37
+@property (nullable)  NSString *foo5;
+@property (nonnull)   NSString *foo6;
+@property (null_unspecified) NSString *foo7;
