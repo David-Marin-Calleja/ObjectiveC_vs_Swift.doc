d430781e130abdf01883c1832c404449340af785
David <david.marin.calleja@gmail.com>
2016-07-26 08:40:03 +0200

    Class

--- code/Class.swift
+++ code/Class.swift
@@ -2,18 +2,24 @@
 import UIKit
 
 class myClass: NSObject {
-    var myProperty:String
-    let MYCONSTANT = "constant"
+    var        myProperty:String
+    let        MYCONSTANT     = "constant"
+    static var staticVariable = "value"
+
+    // Static method
+    static func baz () {
+      print("static function")
+    }
 
     override init() {
-        self.myProperty = ""
+      self.myProperty = ""
     }
 
     func myMethod(myParam: String) -> String {
-        return "in myMethod with param \(myParam)"
+      return "in myMethod with param \(myParam)"
     }
 
     func myOtherMethod() {
-        print("in myOtherMethod")
+      print("in myOtherMethod")
     }
 }
