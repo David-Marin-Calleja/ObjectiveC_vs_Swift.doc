845de44aa3e578c691935f3c4307eb3820e91dc7
David <david.marin.calleja@gmail.com>
2016-08-04 23:22:41 +0200

    array type

--- code/array-type.swift
+++ code/array-type.swift
@@ -2,12 +2,20 @@
 let foo = Array<String>()
 // More often
 let bar = [String]()
-
+// Array literal syntax
 let inmutableArray = [@"foo", @"bar"]
 
 var mutableArray = inmutableArray
 
+// Add values
 mutableArray.append("baz")
+mutableArray.insert("foo", atIndex: 1)
 
 // Array concatenation
 let baz = foo + bar
+
+// Remove values
+mutableArray.removeAtIndex(1)
+
+//
+print(" \(bar[1]) ")
