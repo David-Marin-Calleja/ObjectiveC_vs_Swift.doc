70cae91d155dd1f86d286ddc4ee9634ce61befa0
David <david.marin.calleja@gmail.com>
2016-08-06 14:21:30 +0200

    string

---
+++ code/string-type.swift
@@ -0,0 +1,11 @@
+// concatenation
+let foo = "string"
+let bar = "this is a" + foo + "."
+
+// interpolation
+let foo = "25"
+let bar = "20 + 5 = \(foo) and 2 + 2 = \(2 + 2)"
+
+// String is an array de characters
+let foo:String="value"
+foo.characters
