b9027fba5befd52ccf1610b5c17db5202ee2f921
David <david.marin.calleja@gmail.com>
2016-08-07 15:27:03 +0200

    dictionary

---
+++ code/dictionary-type.swift
@@ -0,0 +1,10 @@
+// Initializing empty Dictionary
+var mutableDictionary = [Int:String]()
+
+let inmutableDictionary = ["ES": "Spain", "US":"United-States"]
+
+
+// Removing
+mutableDictionary["key"]=nil
+
+// Updating
