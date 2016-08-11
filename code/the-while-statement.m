a0c7be93f6db34e506b9dc7b2ba40e02d6616b39
David <david.marin.calleja@gmail.com>
2016-08-11 22:25:19 +0200

    while statement

---
+++ code/the-while-statement.m
@@ -0,0 +1,11 @@
+int count = 5;
+while (count > 0) {
+    NSLog(@"%d", count);
+    count--;
+}
+
+count = 5;
+do {
+    NSLog(@"%d", count);
+    count--;
+} while (count > 0);
--- code/the-while-statement.swift
+++ code/the-while-statement.swift
@@ -1,5 +1,11 @@
 var index = 5
 while index >= 0 {
-    print("\(index)")
-    index = index - 1
+  print("\(index)")
+  index = index - 1
 }
+
+index = 5
+repeat {
+  print("\(index)")
+  index = index - 1
+} while index >= 0
